908fcaba4782b6c6dc73fa5e4aea1a483431ae33
Logan James Kaser <logankaser@fyrn.io>
2018-10-15 18:03:28 -0700

    Basic main

--- src/main.c
+++ src/main.c
@@ -10,19 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "libft.h"
+#include "minishell.h"
+
+extern char	**environ;
+
+void	init_minishell(t_minishell *ms)
+{
+	int	i;
+
+	*ms = (t_minishell){0, 0};
+	i = -1;
+	while(environ[++i])
+		if (!ft_strncmp("USER=", environ[i], 5))
+			ms->user = ft_strdup(environ[i] + 5);
+		else if (!ft_strncmp("PATH=", environ[i], 5))
+			ms->path = ft_strdup(environ[i] + 5);
+}
 
 int	main(void)
 {
 	char *line;
 	int ret;
+	t_minishell ms;
 
+	init_minishell(&ms);
 	line = NULL;
+	ft_printf("%s) ", ms.user);
 	while (1)
 	{
 		ret = get_next_line(0, &line);
-		if (ret > 0)
-			ft_putstr(line);
+		if (!ft_strncmp("path", line, 4))
+			ft_printf("PATH=%s\n", ms.path);
+		ft_printf("%s) ", ms.user);
 		ft_memdel((void**)&line);
 	}
 	return (0);
